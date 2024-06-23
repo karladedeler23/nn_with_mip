01a23cc3555c2ea8be8adf381dda211cd0d81800
Karla Dedeler <kld23@imperial.ac.uk>
2024-06-23 17:41:22 +0100

    Adaptability: settings to enable ssh from home to run expriments at night on lab machines (+ pb with max_correct)

--- tests_running.py
+++ tests_running.py
@@ -7,20 +7,20 @@ hidden_layers = [32]  # Hidden layers configuration
 M = 2.6e4   # Big M constant for ReLU activation constraints (output range)
 margin = 300    # A reasonable margin (for SAT margin) should be a small fraction of this estimated output range
 epsilon = 1.0e-6    # set the precision
-loss_function = 'hinge'  # Choose between 'max_correct', 'hinge', or 'sat_margin'
+loss_function = 'sat_margin'  # Choose between 'max_correct', 'hinge', or 'sat_margin'
 
 size_list = []
 accuracy_train_list = []
 accuracy_test_list = []
 
-for size in range(10, 60, 10):
+for size in range(10, 50, 10):
     sample_size = size  # number of data points
     size_list.append(sample_size)
     # Run the experiments and calculate the average accuracy
     average_accuracy_train, accuracy_test = run_multiple_experiments(num_experiments, sample_size, hidden_layers, M, margin, epsilon, loss_function)
     accuracy_train_list.append(average_accuracy_train)
     accuracy_test_list.append(accuracy_test)
-    # print(f"Average accuracy over {num_experiments} experiments with {sample_size} training points: {average_accuracy_train}")
+    print(f"Average accuracy over {num_experiments} experiments with {sample_size} training points: {average_accuracy_train}")
 
 
 # Plotting the graph
@@ -30,4 +30,7 @@ plt.scatter(size_list, accuracy_test_list, color='b', label='Test Accuracy')
 plt.title(f'Training and test accuracy depending on the training set size using {loss_function} loss function')
 plt.xlabel('Training Set Size')
 plt.ylabel(f'Accuracy')
-plt.show()
+
+# Save the figure with a meaningful name
+file_name = f'training_test_accuracy_{loss_function}_loss.png'
+plt.savefig(file_name)
