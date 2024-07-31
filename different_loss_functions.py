a205fb1435f6d63118bd9efcf8901b5ab42b006a
kld23 <kld23@ic.ac.uk>
2024-07-31 23:19:01 +0100

    config diff loss

--- different_loss_functions.py
+++ different_loss_functions.py
@@ -6,7 +6,7 @@ import os
 
 # Define parameters
 num_experiments = 1
-hidden_layers = [16]  # Hidden layers configuration
+hidden_layers = [32]  # Hidden layers configuration
 M = [785, 785 * hidden_layers[0] + 1]
 margin = M[-1] * 0.01  # A reasonable margin (for SAT margin) should be a small fraction of this estimated output range
 epsilon = 1.0e-1  # set the precision
@@ -27,7 +27,7 @@ W_init = [None for i in range(len(loss_function))]
 b_init = [None for i in range(len(loss_function))]
 
 
-for size in range(10, 40, 10):
+for size in range(10, 50, 10):
     sample_size = size  # number of data points
     size_list.append(sample_size)
     
