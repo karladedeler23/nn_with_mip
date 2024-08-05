a390765e480c06e396833acc15976a32c96177de
kld23 <kld23@ic.ac.uk>
2024-08-05 15:51:18 +0100

    Feature: regression script ready for lab machine

--- regression.py
+++ regression.py
@@ -1,6 +1,7 @@
 from experiments import *
 from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_squared_error
+from datetime import datetime
 
 ########################################################
 
@@ -28,8 +29,19 @@ def load_and_preprocess_data_regression(n, random_nb):
     selected_indices = [i + random_nb for i in range(n)]
     X_train_sample = X_train[selected_indices]
     y_train_sample = y_train[selected_indices]
-    
+    '''
+    print("Shapes:")
+    print(f"Training set: {X_train_sample.shape[0]}")
+    print(f"Testing set: {X_test.shape[0]}")
+   '''
     return X_train_sample, X_test, y_train_sample, y_test
+
+
+def compute_big_M(input_dim, hidden_layers):
+    M = [input_dim+1]
+    for i in range(len(hidden_layers)):
+        M.append(M[i]*hidden_layers[i]+1)
+    return M
 
 ########################################################
 
@@ -109,7 +121,6 @@ def compute_accuracy(X, y, weights, biases):
     y_pred = [item[0] for item in predict_with_mip(weights, biases, X, y)]
     
     # Compute Mean Squared Error
-    n = X.shape[0]
     mse = np.mean((y_pred - y)**2)
     return mse
 
@@ -117,50 +128,87 @@ def compute_accuracy(X, y, weights, biases):
 
 ### MAIN FUNCTION TO RUN THE TRAINING
 
-def main():
-    # Hyperparameters
-    random_nb = np.random.randint(100)
-    print(random_nb)
-    n = 10  # Number of samples to use for training
-    input_dim = 13  # Number of features in the dataset
-    hidden_layers = [8]  # Example hidden layers
-    output_dim = 1  # Single output for regression
-    M = [input_dim + 1, (input_dim + 1) * hidden_layers[0] + 1]  # Big M constant for MIP
-    lambda_reg = 0.0  # Regularization parameter
+def run_regression_mip(current_date_time, num_experiments, sample_size, hidden_layers, random_nb, lambda_reg = 0.0, warm_start = False, W_init = None, b_init = None):
+    training_accuracies, testing_accuracies = [], []
+    runtimes = []
+    nn_config = {'hidden layers': hidden_layers,
+                'training set size' : sample_size,
+                'starting point in the data': random_nb,
+                'loss' : 'mean squared error',                
+                'Regularisation' : lambda_reg,
+                'Warm start' : warm_start
+                }
 
-    # Load and preprocess data
-    X_train, X_test, y_train, y_test = load_and_preprocess_data_regression(n, random_nb)
+    for i in range(num_experiments):
+        # Load and preprocess data
+        X_train, X_test, y_train, y_test = load_and_preprocess_data_regression(sample_size, random_nb+i*sample_size)
+        input_dim = X_train.shape[1]
+        M = compute_big_M(input_dim, hidden_layers)
 
-    # Train the model using MIP
-    runtime, W, b = train_gurobi_model_regression(X_train, y_train, input_dim, hidden_layers, output_dim, M, lambda_reg)
+        # Train Gurobi model and get optimal weights and biases
+        if warm_start and W_init is not None and b_init is not None : 
+            print('warm start')
+            # Using previous weights (when we are actually training with more points)
+            # runtime, W, b = train_gurobi_model_regression_warm_start(X_train, y_train, input_dim, hidden_layers, 1, M, lambda_reg, W_init, b_init)
+        else :
+            print('no warm start')
+            runtime, W, b = train_gurobi_model_regression(X_train, y_train, input_dim, hidden_layers, 1, M, lambda_reg)
+        if W is not None and b is not None:
+            runtimes.append(runtime)
+            print(f"Training completed in {runtime:.2f} seconds. \n")
+            # Compute and print the test accuracy
+            train_mse = compute_accuracy(X_train, y_train, W, b)
+            training_accuracies.append(train_mse)
+            test_mse = compute_accuracy(X_test, y_test, W, b)
+            testing_accuracies.append(test_mse)
+            print("MIP")
+            print(f"Train Mean Squared Error: {train_mse:.2f}")
+            print(f"Test Mean Squared Error: {test_mse:.2f} \n")
+        else:
+            print("Training failed.")
 
-    if runtime is not None:
-        print(f"Training completed in {runtime:.2f} seconds. \n")
-        #print("Weights:", W)
-        #print("Biases:", b)
-        # Compute and print the test accuracy
-        train_mse = compute_accuracy(X_train, y_train, W, b)
-        test_mse = compute_accuracy(X_test, y_test, W, b)
-        print("MIP")
-        print(f"Train Mean Squared Error: {train_mse:.2f}")
-        print(f"Test Mean Squared Error: {test_mse:.2f} \n")
-    else:
-        print("Training failed.")
+    return np.mean(training_accuracies), np.mean(testing_accuracies), W, b, np.mean(runtimes)
     
-    # Train the model using SGDRegressor
-    model_sgd = SGDRegressor(max_iter=200, tol=1e-3)
-    model_sgd.fit(X_train, y_train)
+    
+# Function to train a NN using SGD
+def run_regression_sgd(num_experiments, sample_size, hidden_layers, random_nb):
+    testing_accuracies = []
 
-    # Make predictions
-    y_pred_train_sgd = model_sgd.predict(X_train)
-    y_pred_test_sgd = model_sgd.predict(X_test)
+    for i in range(num_experiments):
+        # Load and preprocess data
+        X_train, X_test, y_train, y_test = load_and_preprocess_data_regression(sample_size, random_nb+i*sample_size)
+        
+        # Train the model using SGDRegressor
+        model_sgd = SGDRegressor(max_iter=1000, tol=1e-3)
+        model_sgd.fit(X_train, y_train)
 
-    # Evaluate performance
-    mse_train_sgd = mean_squared_error(y_train, y_pred_train_sgd)
-    mse_test_sgd = mean_squared_error(y_test, y_pred_test_sgd)
-    print("SGD Regressor")
-    print(f"Train Mean Squared Error: {mse_train_sgd:.2f}")
-    print(f'Test Mean Squared Error: {mse_test_sgd:.2f}\n')
+        # Make predictions
+        y_pred_train_sgd = model_sgd.predict(X_train)
+        y_pred_test_sgd = model_sgd.predict(X_test)
 
-if __name__ == "__main__":
+        # Evaluate performance
+        mse_train_sgd = mean_squared_error(y_train, y_pred_train_sgd)
+        mse_test_sgd = mean_squared_error(y_test, y_pred_test_sgd)
+        testing_accuracies.append(mse_test_sgd)
+        print("SGD Regressor")
+        print(f"Train Mean Squared Error: {mse_train_sgd:.2f}")
+        print(f'Test Mean Squared Error: {mse_test_sgd:.2f}\n')
+
+    return np.mean(testing_accuracies)
+
+########################################################
+
+def main():
+    num_experiments = 1
+    random_nb = 52 #np.random.randint(390)
+    print(random_nb)
+    sample_size = 9  # Number of samples to use for training
+    hidden_layers = [8]  # Example hidden layers
+    lambda_reg = 0.01  # Regularization parameter
+    current_date_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    _ = run_regression_mip(current_date_time, num_experiments, sample_size, hidden_layers, random_nb, lambda_reg = 0.0, warm_start = False, W_init = None, b_init = None)
+    _ = run_regression_sgd(num_experiments, sample_size, hidden_layers, random_nb)
+    return
+
+if __name__ == '__main__':
     main()
